e154c1097a8582753893157f85c4fafd802287a1
rahmaodowa <69065069+rahmaodowa@users.noreply.github.com>
2020-08-11 23:18:54 -0400

    Create codewars1.js

---
+++ codewars1.js
@@ -0,0 +1,15 @@
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+//
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+
+function hello(name) {
+while (name == 'John'){
+  return 'Hello, John!'
+}
+  while (name == 'alice'){
+    return 'Hello, Alice!'
+  }while (!name || name == ''){
+    return 'Hello, World!'
+  }
+  }
